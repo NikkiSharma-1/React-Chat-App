212552953fc9ade1e988f40b26764bac0f6591d5
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-01-14 14:03:47 +0530

    First Commit

---
+++ expense-tracker/src/Store/theme.slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const themeReducer = createSlice({
+  name: "theme",
+  initialState: {
+    darkMode: false,
+  },
+  reducers: {
+    changeTheme(state, action) {
+      console.log(action);
+      state.darkMode=!state.darkMode
+    },
+  },
+});
+
+export const themeActions = themeReducer.actions;
+export default themeReducer.reducer;
